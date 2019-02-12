c51537241c9c4479e35a45258a06951dabd7eda8
AlisherKairolla <lifehero2001@gmail.com>
2019-02-13 02:20:44 +0600

    added

--- Week 1/Task1/ConsoleApp2/ConsoleApp2/Program.cs
+++ Week 1/Task1/ConsoleApp2/ConsoleApp2/Program.cs
@@ -10,49 +10,48 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine()); // read integer n 
+            int n = int.Parse(Console.ReadLine()); //  читаем количество элементов
 
-            int[] a = new int[n]; // creating array a
-            int[] b = new int[n]; // creating array b   
+            int[] a = new int[n]; // создание массива a
+            int[] b = new int[n]; // создание массива b   
 
-            string[] nums = Console.ReadLine().Split(new char[] { ',', ';', '#', ' ' }); //read numbers from input to array of string
+            string[] nums = Console.ReadLine().Split(new char[] { ',', ';', '#', ' ' }); //читаем целую строку с числами и разделяем
 
             for (int i = 0; i < n; i++)
             {
-                a[i] = int.Parse(nums[i]); // converting from string to  array of integers
+                a[i] = int.Parse(nums[i]); // конвертация элементов строки в числа
             }
 
-            int c = 0; // new counter
-            for (int i = 0; i < n; i++)
+            int c = 0; // заводим счетчик
+            for (int i = 0; i < n; i++) // цикл массива
             {
-                for (int j = 1; j <= a[i]; j++)
+                for (int j = 1; j <= a[i]; j++) //цикл для проверки 
                 {
-                    if (a[i] % j == 0) // way of identifying is it prime or no, besides "2"
+                    if (a[i] % j == 0) // способ проверки на простое или нет
                     {
-                        c++; // using counter to count number of primes 
+                        c++; // количество делителей элемента массива   
                     }
                 }
-                if (c == 2) // including "2" to condition
+                if (c == 2) // если 2
                 {
-                    b[i] = a[i];
+                    b[i] = a[i]; //то перекидываем на новый массив
                 }
-                else
-                    b[i] = 0;
-                c = 0;
+                else // делителей много
+                    b[i] = 0; // элемент второго массива приравнимваем нулю
+                c = 0; // обнуляем счетчик
             }
 
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++) // цикл нового массива
             {
-                if (b[i] != 0)
-                    c++;
+                if (b[i] != 0) // если не 0
+                    c++; // то считаем количество элементов нового массива
             }
 
-            Console.WriteLine(c); // output size of array of primes
-
+            Console.WriteLine(c); // вывод размера второго массива
             for (int i = 0; i < n; i++)
             {
-                if (b[i] != 0)
-                    Console.Write($"{b[i]} "); // output array of primes
+                if (b[i] != 0) // если не 0
+                    Console.Write($"{b[i]} "); // то выводим элемент
             }
 
             Console.ReadKey();
